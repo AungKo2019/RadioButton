1107a1ea578169bd4e0b31a9268f0d2c44ed6bd9
AungKo2019 <highflier.ako1991@gmail.com>
2023-04-03 11:43:39 +0630

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
+import { RadioButton } from 'react-native-paper';
 
 export default function App() {
+  const [checked,setChecked]=useState('Apple')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{flexDirection:'row'}}>
+        <RadioButton
+        value='Apple'
+        status={checked==='Apple'?'checked':'unchecked'}
+        onPress={()=>setChecked('Apple')}
+        />
+        <Text style={{color: checked === 'Apple' ? 'red' : 'black'}}>Apple</Text>
+      </View>
+      <View style={{flexDirection:'row'}}>
+        <RadioButton
+        value='Samsung'
+        status={checked==='Samsung'?'checked':'unchecked'}
+        onPress={()=>setChecked('Samsung')}
+        />
+        <Text  style={{color: checked === 'Samsung' ? 'red' : 'black'}}>Samsung</Text>
+      </View>
+      <View style={{flexDirection:'row'}}>
+        <RadioButton
+        value='Oppo'
+        status={checked==='Oppo'?'checked':'unchecked'}
+        onPress={()=>setChecked('Oppo')}
+        />
+        <Text  style={{color: checked === 'Oppo' ? 'red' : 'black'}}>Oppo</Text>
+      </View>
+      
+      
+      <Text style={{marginLeft:20,marginTop:20,fontSize:20,fontWeight:'bold',color:'red'}}>{checked}</Text>
     </View>
+    
   );
 }
 
@@ -14,7 +43,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:50,
+    marginLeft:15,
   },
 });
